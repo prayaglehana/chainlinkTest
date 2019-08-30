18950bff3f91a54cef132a53b3df3bf24fbad29c
Prayag Lehana <prayag.lehana02@gmail.com>
2019-08-30 13:26:53 +0530

    upda

---
+++ app.js
@@ -0,0 +1,28 @@
+import Web3 from './node_modules/web3';
+console.log('saf');
+var fs=require('fs');
+
+var jsonFile = "my.json";
+var parsed= JSON.parse(fs.readFileSync(jsonFile));
+var abi = parsed.abi;
+var _web3;
+var thisAccount;
+
+if (typeof window.web3 !== 'undefined') {
+    _web3 = new Web3(window.web3.currentProvider);
+    console.log('adx');
+    _web3.eth.getAccounts().then(e=>{
+      this.setState({
+        thisAccount:e[0]
+      })
+  });
+          // run the following code only once. Metamask will ask for permission. allow it. then you can comment following code for subsequent ...
+            // window.ethereum.enable().then((account) =>{
+            //     const defaultAccount = account[0]
+            //     _web3.eth.defaultAccount = defaultAccount
+            //     console.log(defaultAccount);
+            
+            // })
+            c=new _web3.eth.Contract(abi, '0x6Ff9A3dc39fB5d857b5Dd97d98e2D0A8eBDB34A5', {from:_web3.eth.defaultAccount})   ;
+            console.log(thisAccount);
+        }
